203fc4007111e69bb7f455447bf9c521c3e76eab
D4rth_ <akiricenko878@gmail.com>
2022-12-02 20:25:23 +0200

    Финальная версия

--- src/main/java/ru/wave/bypassblockedservers/BypassBlockedServers.java
+++ src/main/java/ru/wave/bypassblockedservers/BypassBlockedServers.java
@@ -10,6 +10,7 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 public class BypassBlockedServers implements ClassFileTransformer {
+
     public static void premain(String agentArgs, Instrumentation instrumentation) {
         instrumentation.addTransformer(new BypassBlockedServers());
     }
@@ -19,22 +20,17 @@ public class BypassBlockedServers implements ClassFileTransformer {
             if (className != null) {
                 className = className.replace("/", ".");
                 ClassPool pool = ClassPool.getDefault();
-                if (className.equals("io.netty.bootstrap.Bootstrap") || className.equals("com.mojang.patchy.BlockedServers") || className.contains("BlockedServers")) {
+                if (className.equals("io.netty.bootstrap.Bootstrap") || className.equals("com.mojang.patchy.BlockedServers")) {
                     pool.appendClassPath(new ByteArrayClassPath(className, bytes));
                     CtClass ctClass = pool.get(className);
+
                     setMethodBodyToFalse(ctClass, "isBlockedServerHostName");
                     setMethodBodyToFalse(ctClass, "isBlockedServer");
-                    setMethodBodyToFalse(ctClass, "test");
+
                     return ctClass.toBytecode();
-                } else if (className.equals("com.mojang.patchy.MojangBlockListSupplier") || className.equals("BlockListSupplier")) {
-                    pool.appendClassPath(new ByteArrayClassPath(className, bytes));
-                    CtClass ctClass = pool.get(className);
-                    CtMethod ctMethod = ctClass.getMethod("createBlockList", "");
-                    ctMethod.setBody("{ return null; }");
                 }
             }
-        } catch (Exception ignored) {
-        }
+        } catch (Exception ignored) {}
         return null;
     }
 
@@ -42,7 +38,6 @@ public class BypassBlockedServers implements ClassFileTransformer {
         try {
             CtMethod ctMethod = ctClass.getMethod(methodName, "(Ljava/lang/String;)Z");
             ctMethod.setBody("{ return false; }");
-        } catch (Exception ignored) {
-        }
+        } catch (Exception ignored) {}
     }
 }
